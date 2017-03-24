3ec3bf0546a615fbb7ff0a12a8efa72e8a71f5d4
Oliver Zhang <oli.zhang@gmail.com>
2017-03-24 21:56:35 +0800

    play and pause

--- app/scripts/listen.js
+++ app/scripts/listen.js
@@ -1,9 +1,22 @@
-function listenToStory() {
+function listenToStory(ele) {
 	try {
+		// MARK: - Switch Between two buttons
+		var buttonClass = ele.className;
+		var buttonAction;
+		if (buttonClass.indexOf(' pause') >= 0) {
+			buttonAction = 'pause';
+			buttonClass = buttonClass.replace(' pause', '');
+		} else {
+			buttonAction = 'play';
+			buttonClass += ' pause';
+		}
+		ele.className = buttonClass;
+
 		var title;
 		var text;
-		var currentStory = allstories[gCurrentStoryId];
+		var currentStory;
 		var audioMessage = {};
+		currentStory = allstories[gCurrentStoryId];
 		if (langmode === 'ch' || currentStory.ebody === '') {
 			title = currentStory.cheadline;
 			text = currentStory.cbody;
@@ -16,10 +29,11 @@ function listenToStory() {
 		audioMessage = {
 			title: title,
 			text: text,
-			language: langmode
+			language: langmode,
+			action: buttonAction
 		}
-		console.log (audioMessage);
 		webkit.messageHandlers.listen.postMessage(audioMessage);
+
 	} catch (ignore) {
 
 	}
