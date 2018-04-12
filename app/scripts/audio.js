88b51bb6b9b71299542d5c92983a5802e60e56f5
Oliver Zhang <[oli.zhang@gmail.com]>
2018-04-12 12:39:12 +0800

    audio file

---
+++ app/scripts/audio.js
@@ -0,0 +1,15 @@
+function listenToAudio(slideUrl, slideTitle) {
+       var audioFileUrl = slideUrl.replace(/^.*audio=(.*\.mp3).*$/,'$1');
+       var audioMessage = {
+               title: slideTitle,
+               audioFileUrl: audioFileUrl,
+               interactiveUrl: slideUrl
+       }
+       var eventCategory = 'Listen To Story';
+       try {
+               webkit.messageHandlers.audio.postMessage(audioMessage);
+               ga('send','event',eventCategory, 'Start', slideTitle);
+       } catch (ignore) {
+
+       }
+}
